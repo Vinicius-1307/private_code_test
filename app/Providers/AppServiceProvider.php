1273fd59bce115582beea9eae69a9d8fdaa25258
ViniciusJ <viniciusjose9@outlook.com>
2026-02-06 18:24:43 -0300

    FEAT: starting project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Recipe;
+use App\Policies\RecipePolicy;
+use App\Repositories\CommentRepository;
+use App\Repositories\Interfaces\CommentRepositoryInterface;
+use App\Repositories\Interfaces\RatingRepositoryInterface;
+use App\Repositories\Interfaces\RecipeRepositoryInterface;
+use App\Repositories\RatingRepository;
+use App\Repositories\RecipeRepository;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // Registrar bindings dos Repositories
+        $this->app->bind(RecipeRepositoryInterface::class, RecipeRepository::class);
+        $this->app->bind(CommentRepositoryInterface::class, CommentRepository::class);
+        $this->app->bind(RatingRepositoryInterface::class, RatingRepository::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Registrar Policy
+        Gate::policy(Recipe::class, RecipePolicy::class);
+    }
+}
